1c53afcec9d1ce05fbf9991aa92fb828ab583a80
Salman Farsi <salman.farsi@3together.us>
2024-09-22 22:59:19 -0400

    Added migrations and token generation.

--- WebApiAuth/Controllers/AccountController.cs
+++ WebApiAuth/Controllers/AccountController.cs
@@ -1,41 +1,122 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using WebApiAuth.Models;
 
-namespace WebApiAuth.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountController : ControllerBase
-    {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
-        private readonly IConfiguration _configuration;
+namespace WebApiAuth.Controllers;
 
-        public AccountController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly IConfiguration _configuration;
+
+    public AccountController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _configuration = configuration;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] Register model)
+    {
+        var user = new IdentityUser
         {
-            _userManager = userManager;
-            _roleManager = roleManager;
-            _configuration = configuration;
+            UserName = model.Username,
+            Email = model.Email,
+        };
+
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (result.Succeeded)
+        {
+            return Ok(new { message = "User registered Successfully" });
         }
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] Register model)
+        return BadRequest(result.Errors);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] Login model)
+    {
+        var user = await _userManager.FindByNameAsync(model.Username);
+
+        if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
         {
-            var user = new IdentityUser
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var authClaims = new List<Claim>
             {
-                UserName = model.Username,
-                Email = model.Email,
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName!),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
             };
 
-            var result = await _userManager.CreateAsync(user, model.Password);
+            authClaims.AddRange(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
 
-            if (result.Succeeded)
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                expires: DateTime.Now.AddMinutes(double.Parse(_configuration["Jwt:ExpiryMinutes"]!)),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!)),
+                    SecurityAlgorithms.HmacSha256
+                )
+            );
+
+            return Ok(new
             {
-                return Ok();
+                Token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+        }
+
+        return Unauthorized();
+    }
+
+    [HttpPost("add-role")]
+    public async Task<IActionResult> AddRole([FromBody] string role)
+    {
+        if (!_roleManager.RoleExistsAsync(role).Result)
+        {
+            var newRole = new IdentityRole
+            {
+                Name = role
+            };
+
+            var roleResult = await _roleManager.CreateAsync(newRole);
+
+            if (roleResult.Succeeded)
+            {
+                return Ok(new { message = "Role created successfully" });
             }
 
-            return BadRequest(result.Errors);
+            return BadRequest(roleResult.Errors);
         }
+        return BadRequest(new { message = "Role already exists" });
+    }
+
+    [HttpPost("assign-role")]
+    public async Task<IActionResult> AssignRole([FromBody] UserRole model)
+    {
+        var user = await _userManager.FindByNameAsync(model.Username);
+
+        if (user == null)
+        {
+            return BadRequest(new { message = "User not found" });
+        }
+
+        var result = await _userManager.AddToRoleAsync(user, model.Role);
+
+        if (result.Succeeded)
+        {
+            return Ok(new { message = "Role assigned successfully" });
+        }
+
+        return BadRequest(result.Errors);
     }
 }
---
+++ WebApiAuth/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiAuth.Controllers;
+
+[Authorize(Roles = "User")]
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { message = "This is the User Area" });
+    }
+}
